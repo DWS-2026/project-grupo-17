fda6c365d1535f8be2c47e147dce982242c24143
Lazaro-123 <l.martinezm.2023@alumnos.urjc.es>
2026-03-03 12:53:36 +0100

    controller y hmtl de discotecas

--- ProyectTicketFlow/src/main/java/es/codeurjc/board/controller/DiscotecaController.java
+++ ProyectTicketFlow/src/main/java/es/codeurjc/board/controller/DiscotecaController.java
@@ -2,11 +2,14 @@ package es.codeurjc.board.controller;
 
 import java.io.IOException;
 
+import es.codeurjc.board.model.Discoteca;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 
 import es.codeurjc.board.service.DiscotecaService;
 
@@ -24,26 +27,30 @@ public class DiscotecaController {
         return "discotecas";
     }
 
-    @GetMapping("/discotecas/new")
+    @GetMapping("/discotecas/create-discotecas")
     public String newDiscotecaForm() {
+
         return "create-discotecas";
     }
 
-    @PostMapping("/discotecas/new")
-    public String createDiscoteca(
-            @RequestParam String name,
-            @RequestParam MultipartFile image
-    ) throws IOException {
-
+    @PostMapping("/discotecas/create-discotecas")
+    public String createDiscoteca(@RequestParam String name,
+                                  @RequestParam MultipartFile image) throws IOException {
         discotecaService.save(name, image);
-
         return "redirect:/discotecas";
     }
 
     @GetMapping("/discotecas/{id}/image")
     @ResponseBody
-    public byte[] showImage(@PathVariable long id) {
-        return discotecaService.findById(id).getImage();
+    public ResponseEntity<byte[]> showImage(@PathVariable long id) {
+        Discoteca d = discotecaService.findById(id);
+        if (d == null || d.getImage() == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, "image/jpeg") // importante
+                .body(d.getImage());
     }
 
     @PostMapping("/discotecas/{id}/delete")
