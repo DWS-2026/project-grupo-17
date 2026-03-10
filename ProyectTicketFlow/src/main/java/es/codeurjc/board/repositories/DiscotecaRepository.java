f95be25b6055aba44f1798ddca8b586e44c4203f
Lazaro-123 <l.martinezm.2023@alumnos.urjc.es>
2026-03-10 09:58:51 +0100

    connection of clubs to h2

---
+++ ProyectTicketFlow/src/main/java/es/codeurjc/board/repositories/DiscotecaRepository.java
@@ -0,0 +1,8 @@
+package es.codeurjc.board.repositories;
+
+import es.codeurjc.board.model.Discoteca;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscotecaRepository extends JpaRepository<Discoteca, Long> {
+
+}
