c67c6ead24285d8a62ebc52c6193650cec0f5f27
cabeee <alexcabellomanzanero@gmail.com>
2026-03-13 12:23:47 +0100

    cambio del model de evento

--- ProyectTicketFlow/src/main/java/es/codeurjc/board/model/Evento.java
+++ ProyectTicketFlow/src/main/java/es/codeurjc/board/model/Evento.java
@@ -8,25 +8,23 @@ public class Evento {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     private String name;
-    
+
     @Lob
     private byte[] image;
-    
-    
-    @ManyToOne
-    private Discoteca discoteca; 
-    
-    
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "discoteca_id")
+    private Discoteca discoteca;
+
     private String descripcion;
+
     private Integer edadRequerida;
 
     public Evento() {}
 
-    
-    public Evento(Long id, String name, Discoteca discoteca, String descripcion, byte[] image, Integer edadRequerida) {
-        this.id = id;
+    public Evento(String name, Discoteca discoteca, String descripcion, byte[] image, Integer edadRequerida) {
         this.name = name;
         this.discoteca = discoteca;
         this.descripcion = descripcion;
@@ -34,52 +32,47 @@ public class Evento {
         this.edadRequerida = edadRequerida;
     }
 
-    public Long getId() { 
-        return id; 
+    public Long getId() {
+        return id;
     }
 
-    public void setId(Long id) { 
-        this.id = id; 
+    public String getName() {
+        return name;
     }
 
-    public String getName() { 
-        return name; 
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setName(String name) { 
-        this.name = name; 
+    public byte[] getImage() {
+        return image;
     }
 
-    public byte[] getImage() { 
-        return image; 
+    public void setImage(byte[] image) {
+        this.image = image;
     }
 
-    public void setImage(byte[] image) { 
-        this.image = image; 
+    public Discoteca getDiscoteca() {
+        return discoteca;
     }
 
-    
-    public Discoteca getDiscoteca() { 
-        return discoteca; 
+    public void setDiscoteca(Discoteca discoteca) {
+        this.discoteca = discoteca;
     }
 
-    public void setDiscoteca(Discoteca discoteca) { 
-        this.discoteca = discoteca; 
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public String getDescripcion() { 
-        return descripcion; 
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 
-    public void setDescripcion(String descripcion) { 
-        this.descripcion = descripcion; 
+    public Integer getEdadRequerida() {
+        return edadRequerida;
     }
 
-    public Integer getEdadRequerida() { 
-        return edadRequerida; 
-    }
-
-    public void setEdadRequerida(Integer edadRequerida) { 
-        this.edadRequerida = edadRequerida; 
+    public void setEdadRequerida(Integer edadRequerida) {
+        this.edadRequerida = edadRequerida;
     }
 }
