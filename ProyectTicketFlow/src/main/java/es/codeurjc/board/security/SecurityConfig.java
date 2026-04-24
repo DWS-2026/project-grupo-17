092d6b156accee7d970d86535d4fb66a36972b85
ProPassPlus <paabloleeis@gmail.com>
2026-04-24 13:21:49 +0200

    Ya funciona todo

--- ProyectTicketFlow/src/main/java/es/codeurjc/board/security/SecurityConfig.java
+++ ProyectTicketFlow/src/main/java/es/codeurjc/board/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package es.codeurjc.board.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +36,11 @@ public class SecurityConfig {
                 new DaoAuthenticationProvider(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
